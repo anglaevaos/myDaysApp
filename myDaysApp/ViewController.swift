e5ded5fcded6c3f56fe505f37f15268acebd083b
anglaevaos <anglaevaos@gmail.com>
2024-08-15 22:49:44 +0300

    feat: добавить базовые настройки

--- myDaysApp/ViewController.swift
+++ myDaysApp/ViewController.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  myDaysApp
-//
-//  Created by Oksana Anglaeva on 15.08.2024.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var infoLabel: UILabel!
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+     
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
+    @IBAction func resultButtonTaped(_ sender: UIButton) {
+        infoLabel.text = "You have been enjoing life for \(numberOfDays)!"
+    }
 }
 
